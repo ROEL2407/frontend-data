b40fc75c487fb7aae92e33a6e22ae11d30b08c92
ROEL2407 <roel24komen@gmail.com>
2021-11-09 12:09:29 +0100

    update: extra input for array to filter over later

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ function countStats(pokemonArray) {
         const stats = e.stats;  
         let valueHolder = [];
         let totalValueHolder = 0;
+        var evo = 0;
         stats.forEach(total => {
             // Counts up values of array to a total
             const reducer = (previousValue, currentValue) => previousValue + currentValue; 
@@ -46,11 +47,21 @@ function countStats(pokemonArray) {
             valueHolder.push(total.base_stat);  
             totalValueHolder = valueHolder.reduce(reducer);
         });
+        if (e.naam === "bulbasaur" || e.naam === "charmander" || e.naam === "squirtle") {
+            evo = 1;
+        }
+        else if (e.naam === "ivysaur" || e.naam === "charmeleon" || e.naam === "wartortle") {
+            evo = 2;
+        }
+        else if (e.naam === "venusaur" || e.naam === "charizard" || e.naam === "blastoise") {
+            evo = 3;
+        }
         // Push the name, stats and the total of the stats into a new array
         starterMons.push({
             naam: e.naam,
             stats: e.stats,
-            totaal: totalValueHolder
+            totaal: totalValueHolder,
+            evo: evo
         });
     });
     console.table(starterMons);
