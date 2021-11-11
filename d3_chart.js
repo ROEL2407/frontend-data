d8aeee4675a5fccf387691934b10f9a10065773c
ROEL2407 <roel24komen@gmail.com>
2021-11-11 10:18:49 +0100

    added: scale colors on base of typing

--- d3_chart.js
+++ d3_chart.js
@@ -7,34 +7,34 @@ function d3Chart(starterMons) {
     };
     const width = 800 - margin.left - margin.right;
     const height = 600 - margin.top - margin.bottom;
-    
+
     // Creates sources <svg> element
     const svg = d3.select(".starterStats")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom);
-    
+
     // Group used to enforce margin
     const g = svg.append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`)
         .attr("id", "diagram");
-    
+
     // Global variable for all data
     let data = starterMons;
-    
+
     // Scales setup
     const xscale = d3.scaleLinear().range([0, width]);
     const yscale = d3.scaleBand().rangeRound([0, height]).paddingInner(0.1);
-    
+
     // Axis setup
     const xaxis = d3.axisTop().scale(xscale);
     const g_xaxis = g.append("g").attr("class", "x axis");
     const yaxis = d3.axisLeft().scale(yscale);
     const g_yaxis = g.append("g").attr("class", "y axis");
-    
+
     ///////////////////////
     update(data);
     console.log(data);
-    
+
     function update(new_data) {
         //update the scales
         xscale.domain([0, d3.max(new_data, (d) => d.totaal)]);
@@ -42,7 +42,7 @@ function d3Chart(starterMons) {
         //render the axis
         g_xaxis.transition().call(xaxis);
         g_yaxis.transition().call(yaxis);
-    
+
         const rect = g.selectAll("rect").data(new_data, (d) => d.totaal).join(
             // ENTER 
             // new elements
@@ -58,7 +58,7 @@ function d3Chart(starterMons) {
             // elements that aren't associated with data
             (exit) => exit.remove()
         );
-    
+
         // ENTER + UPDATE
         // both old and new elements
         rect.transition()
@@ -66,52 +66,68 @@ function d3Chart(starterMons) {
             .attr("width", (d) => xscale(d.totaal))
             .attr("y", (d) => yscale(d.naam))
             .attr("class", (d) => d.type);
-    
+
         rect.select("title").text((d) => d.naam);
+
+        //color scale items of types
+        let grass = d3.scaleLinear().domain([1, 10]).range(["white", "green"]);
+        let fire = d3.scaleLinear().domain([1, 10]).range(["white", "red"]);
+        let water = d3.scaleLinear().domain([1, 10]).range(["white", "blue"]);
+
+        rect.selectAll(".grass").attr("fill", function (d) {
+            return grass(d);
+        });
+        rect.selectAll(".fire").attr("fill", function (d) {
+            return fire(d);
+        });
+        rect.selectAll(".water").attr("fill", function (d) {
+            return water(d);
+        });
+
     }
-    
+
     //interactivity
     d3.select("#filter-1st-only").on("change", function () {
         // This will be triggered when the user selects or unselects the checkbox
         const checked = d3.select(this).property("checked");
         if (checked === true) {
             // Checkbox was just checked
-    
+
             // Keep only data element which is the first evolution 
             const filtered_data = data.filter((d) => d.evo === 1);
-    
+
             update(filtered_data); // Update the chart with the filtered data
         } else {
             // Checkbox was just unchecked
             update(data); // Update the chart with all the data we have
         }
     });
-    
+
     d3.select("#filter-2nd-only").on("change", function () {
         // This will be triggered when the user selects or unselects the checkbox
         const checked = d3.select(this).property("checked");
         if (checked === true) {
             // Checkbox was just checked
-    
+
             // Keep only data element which is the first evolution 
             const filtered_data = data.filter((d) => d.evo === 2);
-    
+
             update(filtered_data); // Update the chart with the filtered data
         } else {
             // Checkbox was just unchecked
             update(data); // Update the chart with all the data we have
         }
     });
-    
+
     d3.select("#filter-3rd-only").on("change", function () {
         // This will be triggered when the user selects or unselects the checkbox
         const checked = d3.select(this).property("checked");
         if (checked === true) {
             // Checkbox was just checked
-    
+
             // Keep only data element which is the first evolution 
             const filtered_data = data.filter((d) => d.evo === 3);
-    
+
             update(filtered_data); // Update the chart with the filtered data
         } else {
             // Checkbox was just unchecked
