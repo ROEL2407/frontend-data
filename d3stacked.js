04005f033ee22657ea20ad9ee6d1b38d9349e7d0
ROEL2407 <roel24komen@gmail.com>
2021-11-17 09:39:42 +0100

    added: stacked bar chart code

---
+++ d3stacked.js
@@ -0,0 +1,77 @@
+// main source: https://www.d3-graph-gallery.com/graph/barplot_stacked_basicWide.html
+
+// set the dimensions and margins of the graph
+function d3Stacked(starterMons) {
+  const margin = {
+      top: 10,
+      right: 30,
+      bottom: 20,
+      left: 50
+    },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  const svg = d3.select("#stackedBar")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+  const g = svg.append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  // Parse the Data
+  let dataStacked = starterMons;
+
+  function stacked(dataStacked) {
+
+    // List of subgroups = header of the csv files = soil condition here
+    console.log(dataStacked)
+    const subgroups = dataStacked.slice(1)
+
+    // List of groups = species here = value of the first column called group -> I show them on the X axis
+    const groups = dataStacked.map(d => (d.chain))
+
+    // Add X axis
+    const x = d3.scaleBand()
+      .domain(groups)
+      .range([0, width])
+      .padding([0.2])
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+      .domain([0, 60])
+      .range([height, 0]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // color palette = one color per subgroup
+    const color = d3.scaleOrdinal()
+      .domain(subgroups)
+      .range(['#e41a1c', '#377eb8', '#4daf4a'])
+
+    //stack the data? --> stack per subgroup
+    const stackedData = d3.stack()
+      .keys(subgroups)
+      (dataStacked)
+
+    // Show the bars
+    svg.append("g")
+      .selectAll("g")
+      // Enter in the stack data = loop key per key = group per group
+      .data(stackedData)
+      .join("g")
+      .attr("fill", d => color(d.key))
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(d => d)
+      .join("rect")
+      .attr("x", d => x(d.dataStacked.chain))
+      .attr("y", d => y(d[1]))
+      .attr("height", d => y(d[0]) - y(d[1]))
+      .attr("width", x.bandwidth())
+  }
+stacked(dataStacked);
+}
