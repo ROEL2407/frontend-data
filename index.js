ffa69568c971aaa4229cf61bde57b545b6d15f1b
ROEL2407 <roel24komen@gmail.com>
2021-11-17 09:38:58 +0100

    added: extra if else statement to give evo chain

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ function countStats(pokemonArray) {
         let valueHolder = [];
         let totalValueHolder = 0;
         var evo = 0;
+        var group = 0;
         stats.forEach(total => {
             // Counts up values of array to a total
             const reducer = (previousValue, currentValue) => previousValue + currentValue; 
@@ -60,16 +61,34 @@ function countStats(pokemonArray) {
         else if (e.naam === "venusaur" || e.naam === "charizard" || e.naam === "blastoise") {
             evo = 3;
         }
+
+         // nasty if else statement to give the number of which evolution chain the starter is in
+        // could've made this with an switch case in another function
+        // could also have fetched the evolution chains but in my opinion that isn't important for a few pokemon
+        if (e.naam === "bulbasaur" || e.naam === "ivysaur" || e.naam === "venusaur") {
+            group = 1;
+            
+        }
+        else if (e.naam === "charmander" || e.naam === "charmeleon" || e.naam === "charizard") {
+            group = 2;
+        }
+        else if (e.naam === "squirtle" || e.naam === "wartortle" || e.naam === "blastoise") {
+            group = 3;
+        }
+
+
         // Push the name, stats and the total of the stats into a new array
         starterMons.push({
             naam: e.naam,
             type: e.type,
             stats: e.stats,
             totaal: totalValueHolder,
-            evo: evo
+            evo: evo,
+            chain: group
         });
     });
     d3Chart(starterMons);
+    d3Stacked(starterMons);
 }
 function getPokemon(url) {
     // return every fetch
