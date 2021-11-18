7b450b3f0222f365f0cc4e71ea84a881d32accc8
ROEL2407 <roel24komen@gmail.com>
2021-11-18 11:05:18 +0100

    update: working bars on stacked bar chart, stacking needed

--- d3stacked.js
+++ d3stacked.js
@@ -100,44 +100,31 @@ function d3Stacked(starterMons) {
 
   // Transpose the data into layers
   var dataset = ["grass", "fire", "water"].map(typeName =>data.filter(p => p.type === typeName));
-                      
-  
-  console.log(dataset);
-  // debugger;
-  // console.log(dataset[0]);
+   
   // Set x, y and colors
-  var x = d3.scaleOrdinal()
-    .domain(dataset[0].map(function (d) {
-      return d.xas;
-    }))
+  var x = d3.scaleBand()
+    .domain(["grass", "fire", "water"])
+    //.rangeRoundBands([10, width-10], 0.02);
     .rangeRound([10, width - 10])
     .padding(0.02);
 
   var y = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function (d) {
+    .domain([300, d3.max(dataset, function (d) {
       return d3.max(d, function (d) {
-        return d.yas0 + d.yas;
+        return d.totaal;
       });
     })])
     .range([height, 0]);
 
-  var colors = ["b33040", "#d25c4d", "#f2b447", "#d9d574"];
+  var colors = ["#b33040", "#d25c4d", "#f2b447", "#d9d574"];
 
 
   // Define and draw axes
   var yAxis = d3.axisLeft()
-    .scale(y)
-    .orient("left")
-    .ticks(5)
-    .tickSize(-width, 0, 0)
-    .tickFormat(function (d) {
-      return d
-    });
+    .scale(y);
 
   var xAxis = d3.axisBottom()
-    .scale(x)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%Y"));
+    .scale(x);
 
   svg.append("g")
     .attr("class", "y axis")
@@ -165,81 +152,11 @@ function d3Stacked(starterMons) {
     .enter()
     .append("rect")
     .attr("x", function (d) {
-      return x(d.x);
+      return x(d.type);
     })
-    .attr("y", function (d) {
-      return y(d.y0 + d.y);
-    })
+    .attr("y", d => height - y(d.totaal))
     .attr("height", function (d) {
-      return y(d.y0) - y(d.y0 + d.y);
+      return y(d.totaal);
     })
-    .attr("width", x.rangeBand())
-    .on("mouseover", function () {
-      tooltip.style("display", null);
-    })
-    .on("mouseout", function () {
-      tooltip.style("display", "none");
-    })
-    .on("mousemove", function (d) {
-      var xPosition = d3.mouse(this)[0] - 15;
-      var yPosition = d3.mouse(this)[1] - 25;
-      tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-      tooltip.select("text").text(d.y);
-    });
-
-
-  // Draw legend
-  var legend = svg.selectAll(".legend")
-    .data(colors)
-    .enter().append("g")
-    .attr("class", "legend")
-    .attr("transform", function (d, i) {
-      return "translate(30," + i * 19 + ")";
-    });
-
-  legend.append("rect")
-    .attr("x", width - 18)
-    .attr("width", 18)
-    .attr("height", 18)
-    .style("fill", function (d, i) {
-      return colors.slice().reverse()[i];
-    });
-
-  legend.append("text")
-    .attr("x", width + 5)
-    .attr("y", 9)
-    .attr("dy", ".35em")
-    .style("text-anchor", "start")
-    .text(function (d, i) {
-      switch (i) {
-        case 0:
-          return "Anjou pears";
-        case 1:
-          return "Naval oranges";
-        case 2:
-          return "McIntosh apples";
-        case 3:
-          return "Red Delicious apples";
-      }
-    });
-
-
-  // Prep the tooltip bits, initial display is hidden
-  var tooltip = svg.append("g")
-    .attr("class", "tooltip")
-    .style("display", "none");
-
-  tooltip.append("rect")
-    .attr("width", 30)
-    .attr("height", 20)
-    .attr("fill", "white")
-    .style("opacity", 0.5);
-
-  tooltip.append("text")
-    .attr("x", 15)
-    .attr("dy", "1.2em")
-    .style("text-anchor", "middle")
-    .attr("font-size", "12px")
-    .attr("font-weight", "bold");
-
+    .attr("width", x.bandwidth())
 }
